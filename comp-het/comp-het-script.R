97bd3680221496eabf3a29a7592964e1226b5ec7
Manoj Kanagaraj <manaj999@gmail.com>
2014-11-21 14:57:02 -0500

    functional comp het search. need to generalize, parallelize, and optimize

--- comp-het/comp-het-script.R
+++ comp-het/comp-het-script.R
@@ -33,10 +33,10 @@ step3$proband <- colnames(step4)[ncol(step4)-3]
 
 # prep for sum
   # convert blanks and dots to 0
-step3$CVID1156[step3$CVID1156 == ""] <- 0 # fix step specification
-step3$CVID1156[step3$CVID1156 == "."] <- 0
+step4$CVID1156[step4$CVID1156 == ""] <- 0 # fix step specification
+step4$CVID1156[step4$CVID1156 == "."] <- 0
 
-step3$CVID1156 <- as.numeric(levels(step3$CVID1156))[step3$CVID1156] #needed in order to sum afterwards
+step4$CVID1156 <- as.numeric(levels(step4$CVID1156))[step4$CVID1156] #needed in order to sum afterwards
 
 ## same for other two samples .. change to step3
 step4$X_1738_CVID[step4$X_1738_CVID == ""] <- 0
@@ -47,7 +47,28 @@ step4$X_1738_CVID <- as.numeric(levels(step4$X_1738_CVID))[step4$X_1738_CVID]
 step4$X_1739_CVID <- as.numeric(levels(step4$X_1739_CVID))[step4$X_1739_CVID]
 # do the above 3 steps for all three samples, not one at a time..
 
+
+# filter only variants with proband >=1, change step numbers
+step4<-subset(step3,step3[,10]>0)
+
+# sum over 3 samples. could potentially change this to sum the samples specifically instead of depending upon index
 step4$triosum<-step4[,10]+step4[,11]+step4[,12]
+step4$partial_sum<-step4[,10]+step4[,11]
+
+# filter the three samples only for those with 2
+step5<-subset(step4,step4$triosum ==2)
+
+# filter only recurrent genes
+step6 <- subset(step5, step5$Gene.refgene %in% step5$Gene.refgene[duplicated(step5$Gene.refgene)])
+
+# filter only recurrent genes with a hit in both parents. if this works, then can get rid of partial_sum
+aggregate(step7$X_1738_CVID ~ step7$Gene.refgene, step7, FUN=sum)
+aggregate(step7$X_1739_CVID ~ step7$Gene.refgene, step7, FUN=sum)
+
+aggp1 <- subset(aggregate(step7$X_1739_CVID ~ step7$Gene.refgene, step7, FUN=sum),aggregate(step7$X_1739_CVID ~ step7$Gene.refgene, step7, FUN=sum)[,2]>0)[,1]
+aggp2 <- subset(aggregate(step7$X_1738_CVID ~ step7$Gene.refgene, step7, FUN=sum),aggregate(step7$X_1738_CVID ~ step7$Gene.refgene, step7, FUN=sum)[,2]>0)
+
+step8 <- subset(step7, step7$Gene.refgene %in% aggp1[,1] & step7$Gene.refgene %in% aggp2[,1])
 
 # create new column that sums across pro, p1, p2
 step5 <- apply(step4,1,function(row) sum(vec[ row[ncol(step4-2)] : row[ncol(step4)] ] ))

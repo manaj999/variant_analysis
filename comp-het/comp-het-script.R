7b387adc5ed3d21dfde2f2f7a3422b32b47330f4
Manoj Kanagaraj <manaj999@gmail.com>
2014-12-05 13:39:17 -0500

    fully working. now just edit command line args

--- comp-het/comp-het-script.R
+++ comp-het/comp-het-script.R
@@ -1,101 +1,100 @@
 ### Author: Manoj Kanagaraj
 ### Notes: usage compatible with ANNOVAR refgene variant annotation headers
 
-# set wd
+### EXAMPLE CMD: R comp-het-script <ARGUMENT 1> <ARGUMENT 2> <ARGUMENT 3> <ARGUMENT 4>
+
+### COMMAND LINE ARGUMENTS:
+## ARGUMENT 1: Working Directory
+## ARGUMENT 2: Variant File (headers in same order as example file)
+## ARGUMENT 3: Pedigree File
+## ARGUMENT 4: Output File
+
+## SET FILTER PARAMETERS HERE
+# Control variants from 6500 exomes, etc.
+control_max = 0
+
+# Variants from all patients' parents
+parent_max = 1 # should be at least one since it is a heterozygous variant
+
+# SET WORKING DIRECTORY
 setwd("C:/Users/dave/Desktop/Manoj/desktop/Lab_Documents/2014_Nov")
 
-# input file with all variants to be filtered for CH
+# SET INPUT FILE CONTAINING ANNOVAR OUTPUT
 var = read.csv("CVID_variants_ch_script.csv", header = TRUE)
 
-# input file of the combinations of sample IDs in trios. Pedigree file
+# SET PEDIGREE FILE
 ped = read.csv("pedigree.csv", header = TRUE, stringsAsFactors=FALSE)
 
-# temporary flag
-fam_1 = 0
+# SET OTHER VARIABLES
+output <- data.frame()
 
 ## PART 1: SAMPLE INDEPENDENT
 
 # Sort variants by gene name
 var[order("Gene.refgene")]
 
-# Filter only nonsynonymous or stop variants
+# Filter only nonsynonymous or stopgain/stoploss variants
+# **Edit this step if you wish to include additional variant types**
 step1 <- subset(var, var$ExonicFunc.refgene == "nonsynonymous SNV" | var$ExonicFunc.refgene == "stoploss" | var$ExonicFunc.refgene == "stopgain")
+stopifnot(nrow(step1)>0)
 
-# Filter only variants with contorls of <= 10
-step2 <- subset(step1, step1$control_summaries.3..sum_controls_775max <= 8 & step1$Normal_sum_56 <= 1)
 
+# Filter variants based on rareness using parameters set above
+step2 <- subset(step1, step1[,9] <= control_max & step1[,6] <= parent_max)
+stopifnot(nrow(step2)>0)
 
 
 ## PART 2: FAMILY SPECIFIC
 
-# do all subsequent steps here once ready
+# Repeat for every family to aggregate compound heterozygous events
 for (i in 1:nrow(ped)) {
-  # to refer to a pro, p1, p2, use ped[i,1:3]
- 
+
+  # Extract columns for current trio
+  header = colnames(step2)
+  step3 <- step2[,c(header[1],header[2],header[3],header[4],header[5],header[6],header[7],header[8],header[9],ped[i,1],ped[i,2],ped[i,3])]
   
-
-  # Extract columns for trio (change for pro, p1 and p2)
-  step3 <- step2[,c("chr","Start","Ref","Alt","CVID_sum_217","Normal_sum_56","Gene.refgene","ExonicFunc.refgene","control_summaries.3..sum_controls_775max",ped[i,1],ped[i,2],ped[i,3])]
-
-  # create new column that IDs proband
+  # Create new column to identify trio
   step3$proband <- colnames(step3)[ncol(step3)-2]
 
-  # prep for sum
+  # Clean up data before summing
   for(j in 1:ncol(ped)){
     cur = step3[ped[i,j]]
     step3[ped[i,j]][step3[ped[i,j]] == "." | step3[ped[i,j]] == ""] <- 0
     step3[ped[i,j]] <- lapply(step3[ped[i,j]], function(x) as.numeric(as.character(x)))
   }
  
-  # filter only variants with proband >=1
+  # Filter only variants that occur in proband
   step4<-subset(step3,step3[ped[i,1]]>0)
-  # sum over 3 samples. could potentially change this to sum the samples specifically instead of depending upon index
+  if(nrow(step4)==0) {next}
+  
+  # Sum over the three samples
   step4$triosum<-step4[ped[i,1]]+step4[ped[i,2]]+step4[ped[i,3]]
   
-  # filter the three samples only for those with 2
+  # Filter the three samples only for those with 2. This suggests that it is a heterozygous variant.
   step5<-subset(step4,step4$triosum ==2)
+  if(nrow(step5)==0) {next}
   
-  # filter only recurrent genes
+  # Filter only recurrent genes
   step6 <- subset(step5, step5$Gene.refgene %in% step5$Gene.refgene[duplicated(step5$Gene.refgene)])
-  if(nrow(step6) != 0){ 
-    aggp1 <- subset(aggregate(as.matrix(step6[ped[i,2]]) ~ step6$Gene.refgene, step6, FUN=sum),aggregate(as.matrix(step6[ped[i,2]]) ~ step6$Gene.refgene, step6, FUN=sum)[,2]>0)
-    aggp2 <- subset(aggregate(as.matrix(step6[ped[i,3]]) ~ step6$Gene.refgene, step6, FUN=sum),aggregate(as.matrix(step6[ped[i,3]]) ~ step6$Gene.refgene, step6, FUN=sum)[,2]>0)
-    
-    # filter only compound heterozygous events
-    step7 <- subset(step6, step6$Gene.refgene %in% aggp1[,1] & step6$Gene.refgene %in% aggp2[,1])
-    
-    
-    # clean up output columns
-    # Extract columns for trio (change for pro, p1 and p2)
-    step8 <- step7[,c("chr","Start","Ref","Alt","CVID_sum_217","Normal_sum_56","Gene.refgene","ExonicFunc.refgene","control_summaries.3..sum_controls_775max",ped[i,1],ped[i,2],ped[i,3],"proband")]
-    
-    colnames(step8) <- c("chr","Start","Ref","Alt","CVID_sum_217","Normal_sum_56","Gene.refgene","ExonicFunc.refgene","control_summaries.3..sum_controls_775max","pro","p1","p2","proband")
-    
-    if(fam_1 == 0){
-      output <- step8
-      fam_1 = 1
-    }
-    else{
-      output <- rbind(output,step8)
-    }
-  }
+  if(nrow(step6)==0) {next}
   
-  #file_name <- paste("comp-het_out_",ped[i,1],".csv",sep="")
-  #write.csv(step8, file=file_name, row.names=FALSE)
-#   #write.csv(step8, file="comp-het_out.csv", row.names=FALSE)
-#   if(fam_1 == 0){
-#     output <- step8
-#     fam_1 = 1
-#   }
-#   else{
-#     output <- rbind(output,step8)
-#   }
-  # add col.names=FALSE for successive families
-}
+    
+  # Extract genes with heterozygous variants in each parent
+  aggp1 <- subset(aggregate(as.matrix(step6[ped[i,2]]) ~ step6$Gene.refgene, step6, FUN=sum),aggregate(as.matrix(step6[ped[i,2]]) ~ step6$Gene.refgene, step6, FUN=sum)[,2]>0)
+  aggp2 <- subset(aggregate(as.matrix(step6[ped[i,3]]) ~ step6$Gene.refgene, step6, FUN=sum),aggregate(as.matrix(step6[ped[i,3]]) ~ step6$Gene.refgene, step6, FUN=sum)[,2]>0)
+  
+  # Filter for genes that have recurrent heterozygous events, with at least one from each parent. This suggests that it is a heterozygous variant.
+  step7 <- subset(step6, step6$Gene.refgene %in% aggp1[,1] & step6$Gene.refgene %in% aggp2[,1])
+  
+  
+  # Extract relevant columns (exclude "triosum" column)
+  step7 <- step7[ - c(14)]
+  
+  colnames(step7)[10:12] <- c("pro","p1","p2")
+  
+  # Use output df to aggregate compound heterozygous events from all families
+  output <- rbind(output,step7)
+}  
 
-# figure out output 
-
-# output file
+# Output file
 write.table(output, file = "comp-het_out.csv",row.names=FALSE, sep=",")
-# write.csv(TADA_gene_list, file="Varscan_All-Trios_2_prepped-vars.csv")
-
